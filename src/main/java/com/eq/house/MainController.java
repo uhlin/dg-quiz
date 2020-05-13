038b27d92e8107b6cbacd89a0fd2032d99d280fb
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-13 23:11:32 +0200

    Added MainController  --  initial commit

---
+++ src/main/java/com/eq/house/MainController.java
@@ -0,0 +1,17 @@
+package com.eq.house;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/createQuiz")
+	public String createQuiz() {
+		return "createQuiz";
+	}
+}
