dc3a5cfb8550cd65f6d58c5afe5a0e631197c15d
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-13 18:07:18 +0200

    Initial commit

---
+++ src/main/java/com/eq/house/DgQuizApplication.java
@@ -0,0 +1,13 @@
+package com.eq.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eq/house/DgQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eq.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
