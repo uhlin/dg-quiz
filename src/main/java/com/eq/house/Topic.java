cdba8fd44afcf6cea797cd59dc6beb52f7fb5c3e
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-13 23:02:26 +0200

    Added Topic.java

---
+++ src/main/java/com/eq/house/Topic.java
@@ -0,0 +1,19 @@
+package com.eq.house;
+
+public enum Topic {
+	Sports,
+	GamesAndLeisure,
+	History,
+	Culture,
+	Literature,
+	Music,
+	FoodAndDrinks,
+	AnimalsAndNature,
+	Entertainment,
+	Language,
+	MoviesAndTVShows,
+	Geography,
+	Society,
+	TechnologyAndScience,
+	Other
+}
