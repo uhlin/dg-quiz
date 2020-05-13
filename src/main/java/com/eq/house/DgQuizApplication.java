bb280d0f1ab3ee3779a56efc40a0bf18dcae8f02
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-13 21:45:59 +0200

    Exclude SecurityAutoConfiguration

--- src/main/java/com/eq/house/DgQuizApplication.java
+++ src/main/java/com/eq/house/DgQuizApplication.java
@@ -3,7 +3,9 @@ package com.eq.house;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class DgQuizApplication {
 
 	public static void main(String[] args) {
