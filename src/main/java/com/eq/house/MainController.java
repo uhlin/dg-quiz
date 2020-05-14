1562c707dcf3deca00f76c282808a832c62bf7b1
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-14 14:49:18 +0200

    Added code

--- src/main/java/com/eq/house/MainController.java
+++ src/main/java/com/eq/house/MainController.java
@@ -1,7 +1,10 @@
 package com.eq.house;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -10,8 +13,19 @@ public class MainController {
 		return "index";
 	}
 
-	@RequestMapping("/createQuiz")
+	@GetMapping("/createQuiz")
 	public String createQuiz() {
 		return "createQuiz";
+	}
+
+	@PostMapping("/createQuiz")
+	public String createQuizBegin(
+			@RequestParam(name = "quizTitle") String  title,
+			@RequestParam(name = "quizTopic") Integer topic,
+			@RequestParam(name = "quizLang")  Integer lang) {
+		System.out.println("quizTitle: " + title);
+		System.out.println("quizTopic: " + topic);
+		System.out.println("quizLang:  " + lang);
+		return "addQuestion";
 	}
 }
