814878da89b9f2137a243a955a60a7df56e2111f
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-22 15:14:29 +0200

    Lade till kod

--- src/main/java/com/eq/house/Question.java
+++ src/main/java/com/eq/house/Question.java
@@ -1,9 +1,61 @@
 package com.eq.house;
 
 public class Question {
-	QuestionType qType;
+	private QuestionType qType;
 
-	//QuestionText qText;
-	//QuestionSound qSound;
-	//QuestionImage qImage;
+	private QuestionText qText;
+	private QuestionSound qSound;
+	private QuestionImage qImage;
+
+	public Question() {
+		this.qType = QuestionType.None;
+		this.qText = null;
+		this.qSound = null;
+		this.qImage = null;
+		System.out.println("** Question class: default constructor called **");
+	}
+
+	public Question(QuestionType qType) {
+		this.qType = qType;
+		this.qText = null;
+		this.qSound = null;
+		this.qImage = null;
+	}
+
+/***************************************************
+ *
+ * Getters/setters
+ *
+ ***************************************************/
+
+	public QuestionType getqType() {
+		return qType;
+	}
+	public void setqType(QuestionType qType) {
+		this.qType = qType;
+	}
+
+
+	public QuestionText getqText() {
+		return qText;
+	}
+	public void setqText(QuestionText qText) {
+		this.qText = qText;
+	}
+
+
+	public QuestionSound getqSound() {
+		return qSound;
+	}
+	public void setqSound(QuestionSound qSound) {
+		this.qSound = qSound;
+	}
+
+
+	public QuestionImage getqImage() {
+		return qImage;
+	}
+	public void setqImage(QuestionImage qImage) {
+		this.qImage = qImage;
+	}
 }
