c526e6948e86b9a61f2d13c62992a693f6f42e78
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-22 16:20:05 +0200

    Lade till getQuestion() respektive getAllQuestionsForQuiz()

--- src/main/java/com/eq/house/MainController.java
+++ src/main/java/com/eq/house/MainController.java
@@ -330,6 +330,63 @@ public class MainController {
 
 	/* -------------------- Play functionality -------------------- */
 
+	public Question getQuestion(final String quizId, final Integer questionNum) {
+		Question question = null;
+		Iterable<QuestionText> allText = questionTextRepo.findAll();
+		Iterable<QuestionSound> allSound = questionSoundRepo.findAll();
+		Iterable<QuestionImage> allImage = questionImageRepo.findAll();
+
+		for (QuestionText qText : allText) {
+			if (qText.getQuestionNum() == questionNum) {
+				question = new Question(QuestionType.Text);
+				question.setqText(qText);
+				return question;
+			}
+		}
+
+		for (QuestionSound qSound : allSound) {
+			if (qSound.getQuestionNum() == questionNum) {
+				question = new Question(QuestionType.Sound);
+				question.setqSound(qSound);
+				return question;
+			}
+		}
+
+		for (QuestionImage qImage : allImage) {
+			if (qImage.getQuestionNum() == questionNum) {
+				question = new Question(QuestionType.Image);
+				question.setqImage(qImage);
+				return question;
+			}
+		}
+
+		/* not found! */
+		System.err.println("----- getQuestion: -----");
+		System.err.println("quizId:      " + quizId);
+		System.err.println("questionNum: " + questionNum);
+		System.err.println("CANNOT TO FIND QUESTION!!!");
+		return null;
+	}
+
+	public List<Question> getAllQuestionsForQuiz(final String quizId) {
+		Quiz quiz = getQuizByUniqueId(quizId);
+		List<Question> questions = new LinkedList<Question>();
+
+		for (Integer i = 1; i <= quiz.getNumQuestions(); i ++) {
+			Question q = getQuestion(quizId, i);
+			if (q != null)
+				questions.add(q);
+		}
+
+		if (questions.size() != quiz.getNumQuestions()) {
+			System.err.println("----- getAllQuestionsForQuiz -----");
+			System.err.println("fatal: size mismatch!");
+			return null;
+		}
+
+		return questions;
+	}
+
 	@GetMapping("/playQuiz")
 	public String playQuiz(
 			Model model,
