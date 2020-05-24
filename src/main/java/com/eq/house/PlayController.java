954fdb54d57244e2009f627b7b47dd009274f82b
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-24 14:54:01 +0200

    GetMapping -> PostMapping (/askQuestion)

--- src/main/java/com/eq/house/PlayController.java
+++ src/main/java/com/eq/house/PlayController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -141,7 +142,7 @@ public class PlayController {
 		return "playQuiz";
 	}
 
-	@GetMapping("/askQuestion")
+	@PostMapping("/askQuestion")
 	public String askQuestion(
 			Model model,
 			@RequestParam(name = "quizId", required = true) String quizId,
