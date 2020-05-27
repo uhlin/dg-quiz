b76b6877f9258ce789d0810b3a6d372ff9b0a5a0
Markus Uhlin <markus.uhlin@bredband.net>
2020-05-27 09:43:09 +0200

    Lade till findByPlayerId() och deleteByPlayerId()

--- src/main/java/com/eq/house/AnswerRepo.java
+++ src/main/java/com/eq/house/AnswerRepo.java
@@ -1,10 +1,16 @@
 package com.eq.house;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 //This will be AUTO IMPLEMENTED by Spring
 //CRUD refers Create, Read, Update, Delete
 
 public interface AnswerRepo extends CrudRepository<Answer, Long> {
-	;
+
+	List<Answer> findByPlayerId(String playerId);
+
+	void deleteByPlayerId(String playerId);
+
 }
