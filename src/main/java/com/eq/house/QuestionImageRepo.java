c0dc3ce92a3e6c887ea0707dfeee74ba7f997a9e
Markus Uhlin <markus.uhlin@bredband.net>
2020-06-04 14:59:00 +0200

    Lade till findByQuizId() och deleteByQuizId()

--- src/main/java/com/eq/house/QuestionImageRepo.java
+++ src/main/java/com/eq/house/QuestionImageRepo.java
@@ -1,10 +1,16 @@
 package com.eq.house;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 //This will be AUTO IMPLEMENTED by Spring
 //CRUD refers Create, Read, Update, Delete
 
 public interface QuestionImageRepo extends CrudRepository<QuestionImage, Long> {
-	;
+
+	List<QuestionImage> findByQuizId(String quizId);
+
+	void deleteByQuizId(String quizId);
+
 }
