93f230afedeb94fe82b4ac97db4f10270b8cf623
Markus Uhlin <markus.uhlin@bredband.net>
2020-06-23 13:53:15 +0200

    ReportSubject enum  --  initial revision

---
+++ src/main/java/com/eq/house/ReportSubject.java
@@ -0,0 +1,16 @@
+package com.eq.house;
+
+public enum ReportSubject {
+
+	Abuse,
+	FalseNews,
+	Harassment,
+	HateSpeech,
+	Nudity,
+	PromotingDrugs,
+	ScamOrFraud,
+	Spam,
+	Terrorism,
+	Violence
+
+}
