ae4f01fd80b8673b7c8bcc466b1e8bb0d581800a
Markus Uhlin <markus.uhlin@bredband.net>
2020-06-30 17:48:07 +0200

    Lade till kod

--- src/main/resources/static/scripts/answerQuestion.js
+++ src/main/resources/static/scripts/answerQuestion.js
@@ -1,4 +1,18 @@
 function answerQuestion() {
+	var answers = document.getElementsByName("answer");
+
+	if (answers.length != 0) {
+		document.getElementById("opt1Checked").value = answers[0].checked;
+		document.getElementById("opt2Checked").value = answers[1].checked;
+		document.getElementById("opt3Checked").value = answers[2].checked;
+		document.getElementById("opt4Checked").value = answers[3].checked;
+
+		if (answers.length == 6) {
+			document.getElementById("opt5Checked").value = answers[4].checked;
+			document.getElementById("opt6Checked").value = answers[5].checked;
+		}
+	}
+
 	document.getElementById("ansQuestionForm").action = "/answerQuestion";
 	document.getElementById("ansQuestionForm").onsubmit = "";
 	document.getElementById("ansQuestionForm").submit();
